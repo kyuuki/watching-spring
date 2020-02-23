ef6a29f76eb7cd716d2c906fd93f66aa5c75c69b
kyuuki <kyuuki.japan@gmail.com>
2020-02-23 16:03:38 +0900

    Add 3 API

--- src/main/kotlin/jp/kyuuki/watching/spring/RootController.kt
+++ src/main/kotlin/jp/kyuuki/watching/spring/RootController.kt
@@ -1,13 +1,33 @@
 package jp.kyuuki.watching.spring
 
+import jp.kyuuki.watching.spring.model.Event
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod.*
 
 @RestController
+@RequestMapping("/v1")
 class RootController {
+    companion object {
+        private val logger = LoggerFactory.getLogger(RootController::class.java)
+    }
 
-    @RequestMapping("/")
-    fun root(): String {
-        return "{ \"message\": \"Hello world\" }";
+    @RequestMapping("/users", method = [ POST ])
+    fun postUsers(): Map<String, Any> {
+        logger.info("postUsers")
+        return mapOf("id" to 1234, "api_key" to "ABCDEFG")
+    }
+
+    @RequestMapping("/users", method = [ PUT ])
+    fun putUsers(): Map<String, Int> {
+        logger.info("putUsers")
+        return mapOf()
+    }
+
+    @RequestMapping("/events", method = [ GET ])
+    fun getEvents(): Array<Event> {
+        logger.info("getEvents")
+        return arrayOf(Event(1234, "おはよう"), Event(5678, "おやすみ"), Event(5678, "おはよう"))
     }
 }
---
+++ src/main/kotlin/jp/kyuuki/watching/spring/model/Event.kt
@@ -0,0 +1,3 @@
+package jp.kyuuki.watching.spring.model
+
+data class Event(val user_id: Int, val description: String)
