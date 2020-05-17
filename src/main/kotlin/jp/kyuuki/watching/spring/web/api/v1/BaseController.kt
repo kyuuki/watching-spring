f1c707fdb0b5f40cc363b71a61ef042fa1d57e99
kyuuki <kyuuki.japan@gmail.com>
2020-05-17 14:01:47 +0900

    Return 404 error when not finding user

--- src/main/kotlin/jp/kyuuki/watching/spring/web/api/v1/BaseController.kt
+++ src/main/kotlin/jp/kyuuki/watching/spring/web/api/v1/BaseController.kt
@@ -1,5 +1,6 @@
 package jp.kyuuki.watching.spring.web.api.v1
 
+import javassist.NotFoundException
 import jp.kyuuki.watching.spring.component.AuthenticationComponent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.ResponseStatus
 open class BaseController {
     @Autowired
     lateinit var authenticationComponent: AuthenticationComponent
+
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleException(e: NotFoundException): Map<String, String> {
+        return mapOf("message" to e.localizedMessage)
+    }
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
